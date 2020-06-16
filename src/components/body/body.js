7fb1c071b3c7ce9a2ee3fdf9f78c1a4120996b5b
ArtemChumanov <artem_chumanov@ukr.net>
2020-06-16 14:20:30 +0300

    get data

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,10 +1,14 @@
 import React from "react";
 import {Grid} from "@material-ui/core";
 import "./body.css"
+import ListProducts from "../../containers/ListProducts";
+import ProductsList from "../products-list/products-list";
 
 const Body=()=>{
     return(
-        <Grid className="block-body"></Grid>
+        <Grid className="block-body">
+            <ProductsList/>
+        </Grid>
 
 
     )
---
+++ src/components/products-list-item/products-list-item.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductsListItem=({item})=>{
+    return(
+        <div>
+            <img src={item.img}/>
+            <h3></h3>
+            <span></span>
+        </div>
+    )
+}
+export default ProductsListItem;

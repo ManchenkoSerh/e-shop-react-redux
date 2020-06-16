1805a3bd1d7b0b507b79704cb1232f69b8937ec9
Serhii Manchenko <grandx.go@gmail.com>
2020-06-16 14:10:08 +0300

    delete lib mui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Header from "./components/header/header";
-import Body from "./components/body/body";
-import Footer from "./components/footer/footer";
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <Body/>
-        <Footer/>
+    <div>
+      hello
     </div>
   );
 }
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,4 +1,38 @@
-const initialState=[];
+const initialState={
+    categories: [
+        {
+            titleCategory: 'phone',
+            items: [
+                {
+                    titleItem: 'Iphone',
+                    model: 'X',
+                    price: '499$',
+                    description: '',
+                    img: ''
+                },
+                {
+                    titleItem: 'Samsung',
+                    model: 'S20',
+                    price: '599$',
+                    description: '',
+                    img: ''
+                }
+            ]
+        },
+        {
+            titleCategory: 'laptop',
+            items: [
+                {
+                    titleItem: 'MacBook',
+                    model: 'Air 13',
+                    price: '1399$',
+                    description: '',
+                    img: ''
+                }
+            ]
+        }
+    ]
+};
 
 function data(state = initialState, action) {
     switch (action.type) {

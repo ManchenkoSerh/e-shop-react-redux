96eba4c724be327a662d7c8ba1d2697418632fb3
ArtemChumanov <artem_chumanov@ukr.net>
2020-06-17 12:59:29 +0300

    fetch products,routers

--- src/components/products-list-items/products-list-items.js
+++ src/components/products-list-items/products-list-items.js
@@ -1,14 +1,17 @@
 import React from "react";
+import {Button, ButtonInfo, Img, Item} from "./products-list-items-style";
+import {Link} from "react-router-dom";
 
 
-const ProductsListItem=({item})=>{
+const ProductsListItem=({item,onIncrease})=>{
     return(
-        <div>
-            <img src={item.img}/>
+        <Item>
+            <Img src={item.img}/>
             <h3>{item.titleItem} <span>{item.model}</span></h3>
-            <span>{item.price}</span>
-            <button>Купити</button>
-        </div>
+            <span>{item.price}</span><br/>
+            <Link to="/shopping-cart"><Button onclick={onIncrease}>Купити</Button></Link>
+            <Link to="/info"><ButtonInfo>Детальніше</ButtonInfo></Link>
+        </Item>
     )
 }
 export default ProductsListItem

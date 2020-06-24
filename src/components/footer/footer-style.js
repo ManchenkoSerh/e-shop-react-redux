37bfb4e2d589a03606109cfdea5f10308ade2ffd
Serhii Manchenko <grandx.go@gmail.com>
2020-06-24 10:40:29 +0300

    change footer style

--- src/components/footer/footer-style.js
+++ src/components/footer/footer-style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const GeneralInfo = styled.div`
   display: flex;
   flex-wrap: wrap;
+  padding: 20px;
 `;
 export const SellerBlock = styled.div`
   width: 30%;
